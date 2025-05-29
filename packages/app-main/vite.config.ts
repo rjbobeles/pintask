85b6e180a44d63519dff10f7888fdc00d3ea62f6
Ryan Jandrick Obeles <51547772+rjbobeles@users.noreply.github.com>
2025-05-29 19:03:32 +0800

    fix: change port to 3000

--- packages/app-main/vite.config.ts
+++ packages/app-main/vite.config.ts
@@ -6,11 +6,11 @@ export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/app-main',
   server: {
-    port: 4200,
+    port: 3000,
     host: 'localhost',
   },
   preview: {
-    port: 4300,
+    port: 3000,
     host: 'localhost',
   },
   plugins: [react()],
