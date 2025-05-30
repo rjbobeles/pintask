f802d733682a46ff32e5f470cf440c8f9451a315
Ryan Jandrick Obeles <51547772+rjbobeles@users.noreply.github.com>
2025-05-31 06:42:41 +0800

    feat: favicon, font, logo assets

--- packages/app-main/tailwind.config.js
+++ packages/app-main/tailwind.config.js
@@ -1,11 +1,16 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind')
 const { join } = require('path')
+
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'), ...createGlobPatternsForDependencies(__dirname)],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        varela: ['"Varela Round"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
