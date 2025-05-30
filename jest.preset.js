cfbb3ffbe32cc40712536e70baf1dec18bdf1f69
Ryan Jandrick Obeles <51547772+rjbobeles@users.noreply.github.com>
2025-05-30 14:25:58 +0800

    fix: test-setup

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,8 @@
 const nxPreset = require('@nx/jest/preset').default
 
-module.exports = { ...nxPreset }
+module.exports = {
+  ...nxPreset,
+  bail: false,
+  collectCoverage: false,
+  coverageReporters: ['clover', 'json', 'text', 'lcov', 'text-summary', 'html'],
+}
